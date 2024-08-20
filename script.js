7e8764cf945a974d948c79d285f5d2e4be01310a
Edward Omopariola <femiedjr@gmail.com>
2024-08-20 07:18:54 -0400

    change on js function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const array = [2, 4, 6, 4, 7, 9]
 
 function sumOfArray(intergers) {
-    if(intergers.length === 1) { // if array.length is equal to 1 return that Element
+    if(intergers.length === 1) { // if integers.length is equal to 1 
         return intergers[0];
     }
     else {
